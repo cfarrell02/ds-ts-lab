6d700fbb4edc6c24efb8882316bb7316b26ce7e4
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2023-09-18 11:20:19 +0100

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -22,6 +22,17 @@ const friend1 = {
   console.log(friends[1]);
   
   //   -------------------
+
+
+    interface Colleague {
+        name: string;
+        department: string;
+        contact: {
+            email: string;
+            extension: number;
+        }
+    };
+    
   const colleague1 = {
     name: "Ralph Graham",
     department: "Engineering",
